89b2e30d8b0bed9055e0ce35e7caa207e9793a63
eyi1 <ej7729@gmail.com>
2018-03-28 01:13:32 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,16 @@
 def using_push (array, int)
 array.push(int)
 end
+
+def using_unshift (array, string)
+array.unshift(string)
+end
+
+def using_pop (array)
+  array.pop
+end 
+
+def pop_with_args (arr)
+arr.pop
+arr.pop
+end 
